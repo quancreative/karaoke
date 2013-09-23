2c1c250d044cbbacb197d782be2d112a80226f46
qngo-windwalker <quan.ngo@windwalker.com>
2013-09-23 18:27:29 -0400

    Working.

--- dev/python/2.7src/wx_karaoke.py
+++ dev/python/2.7src/wx_karaoke.py
@@ -10,9 +10,6 @@ import sys
 import requests
 import wx # 2.8
 
-# from firebase import firebase
-from firebase import firebase
-
 # http://www.crummy.com/software/BeautifulSoup/
 # After downloading the package and cmd to the director with the install script (setup.py), run: ..>\Python33\python setup.py install
 # Doc: http://www.crummy.com/software/BeautifulSoup/bs4/doc/
@@ -21,7 +18,7 @@ from firebase import firebase
 class Karaoke(wx.Frame):
     
     def __init__(self, master=None):
-        wx.Frame.__init__(self, None, pos=wx.DefaultPosition, size=(1024, 768))
+        wx.Frame.__init__(self, None, pos=wx.DefaultPosition, size=(300, 300))
         
         self.currentSong = {'src' : 'musics/Vietnamese/Ai Lên Xứ Hoa Đào - Sơn Ca.VOB', 'track' : 2}
         
@@ -32,24 +29,26 @@ class Karaoke(wx.Frame):
         self.videopanel = wx.Panel(self, 1, style=wx.STAY_ON_TOP)
         self.videopanel.SetBackgroundColour(wx.BLACK)
         
+        # The second panel holds controls
+        ctrlpanel = wx.Panel(self, -1 )
+        next  = wx.Button(ctrlpanel, label="Next")
+        
+        self.Bind(wx.EVT_BUTTON, self.onNext, next)
+        
         sizer = wx.BoxSizer(wx.VERTICAL)
         sizer.Add(self.videopanel, 1, flag=wx.EXPAND | wx.TOP)
+        sizer.Add(ctrlpanel, flag=wx.EXPAND | wx.BOTTOM)
         self.SetSizer(sizer)
         self.SetMinSize((350, 300))
         
         self.instance = vlc.Instance()
         self.player = self.instance.media_player_new()
         
+        
         # VLC Events
         self.vlcEvents = self.player.event_manager()
         self.vlcEvents.event_attach(vlc.EventType.MediaPlayerEndReached, self.onEndReached)
-        
-        if sys.platform == "linux2": # for Linux using the X Server
-            self.player.set_xwindow(self.videopanel.GetHandle())
-        elif sys.platform == "win32": # for Windows
-            self.player.set_hwnd(self.videopanel.GetHandle())
-        elif sys.platform == "darwin": # for MacOS
-            self.player.set_agl(self.videopanel.GetHandle())
+        self.vlcEvents.event_attach(vlc.EventType.MediaPlayerVout, self.onMediaVout)
         
 #         self.ToggleWindowStyle(wx.STAY_ON_TOP)
         
@@ -71,7 +70,7 @@ class Karaoke(wx.Frame):
 #         from json import JSONEncoder
 #         songDict = json.dumps(result, cls=JSONEncoder)
         
-        r = requests.get('https://karaoke.firebaseio.com/playlist.json')
+        r = requests.get('https://karaoke.firebaseio.com/playlist.json', verify=False)
         songDict = r.json()
 
 #         print songDict
@@ -82,6 +81,9 @@ class Karaoke(wx.Frame):
         self.firstSongOjbKey = listSorted[0]
         # Get the first song data
         songObj = songDict[self.firstSongOjbKey]
+        
+#         unicodeSongObjSrc = songObj['src']
+#         unicodeCurrentSongSrc = unicode(self.currentSong['src']) 
         
         if songObj['src'] != self.currentSong['src'] :
             # Play new song
@@ -97,21 +99,40 @@ class Karaoke(wx.Frame):
         normalPath = os.path.normpath('../../../' + str(self.currentSong['src']))
 #         songName = os.path.basename(normalPath)
 #         self.master.title(str(songName))
-                
+
         # Init VLC
         self.player.stop()
-        self.player.set_media(self.instance.media_new(normalPath))
-        self.player.audio_set_track(self.currentSong['track'])
-        self.player.play()
         
+        self.vlcEvents.event_attach(vlc.EventType.MediaPlayerTimeChanged, self.onMediaTimeChanged)
+        self.player.set_media(self.instance.media_new(normalPath))
+        self.player.play()
+    
+    def onMediaVout(self, evt):
+        if sys.platform == "linux2": # for Linux using the X Server
+            self.player.set_xwindow(self.videopanel.GetHandle())
+        elif sys.platform == "win32": # for Windows
+            self.player.set_hwnd(self.videopanel.GetHandle())
+        elif sys.platform == "darwin": # for MacOS
+            self.player.set_agl(self.videopanel.GetHandle())
+            
+    def onMediaTimeChanged(self, evt):
+        if self.player.audio_get_track() != int(self.currentSong['track']) :
+            self.player.audio_set_track(self.currentSong['track'])
+        else :
+            self.vlcEvents.event_detach(vlc.EventType.MediaPlayerTimeChanged)
+            
     def onEndReached(self, evt):
         self.deleteFistSong()
         print(evt)
         
     def deleteFistSong(self):
-        fb = firebase.FirebaseApplication('https://karaoke.firebaseio.com/', None)
-        fb.delete('/playlist', self.firstSongOjbKey)
-        
+#         fb = firebase.FirebaseApplication('https://karaoke.firebaseio.com/', None, {'verify' : False})
+#         fb.delete('/playlist', self.firstSongOjbKey)
+        r = requests.delete('https://karaoke.firebaseio.com/playlist/' + self.firstSongOjbKey + '.json', verify=False)
+
+    def onNext(self, evt):
+        self.deleteFistSong()
+                
     def onExit(self, evt):
         """Closes the window.
         """
